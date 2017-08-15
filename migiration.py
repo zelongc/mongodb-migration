e3eb889fd8f9faab38a324f6ff4bdb1a01c80a82
Zelong Cong <zelongc@student.unimelb.edu.au>
2017-08-15 16:08:06 +1000

    bug fix

--- migiration.py
+++ migiration.py
@@ -3,33 +3,30 @@ import argparse
 import datetime
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--fromaddress", required=True, help="The access tokens")
-ap.add_argument("-d", "--toaddress", required=True, help="The access tokens")
-ap.add_argument("-fd", "--FromDB", required=True)
-ap.add_argument("-fc", "--FromCollection", required=True)
-ap.add_argument("-td", "--ToDB", required=True)
-ap.add_argument("-fc", "--ToCollection", required=True)
+ap.add_argument("-f", "--fromaddress", required=True)
+ap.add_argument("-t", "--toaddress", required=True)
+ap.add_argument("-d", "--DB", required=True)
+ap.add_argument("-c", "--Collection", required=True)
+
 
 args = vars(ap.parse_args())
 
 # Parse the arguments
 from_address = args["toaddress"]
 to_address = args["fromaddress"]
-from_db = args["FromDB"]
-to_db = args["ToDB"]
-from_collection = args["FromCollection"]
-to_collection = args["ToCollection"]
+db=args["DB"]
+collection=["Collection"]
 
 
 class db_client(object):
     def __init__(self):
         self.from_client = MongoClient('mongodb://%s:27017/' % from_address)
-        self.from_db = self.from_client['%s' % from_db]
-        self.from_collection = self.from_db['%s' % from_collection]
+        self.from_db = self.from_client['%s' % db]
+        self.from_collection = self.from_db['%s' % collection]
 
         self.to_client = MongoClient('mongodb://%s:27017/' % to_address)
-        self.to_db = self.to_client["%s" % to_db]
-        self.to_collection = self.to_db["%s" % to_collection]
+        self.to_db = self.to_client["%s" % db]
+        self.to_collection = self.to_db["%s" % collection]
 
     def run(self):
         try:
@@ -43,4 +40,4 @@ class db_client(object):
 
 if __name__ == "__main__":
     Client = db_client()
-    db_client.run()
+    Client.run()
